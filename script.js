15abeb76621c17e446c92d43d2cdbabc5a4217ca
Julian <julianrinaudo18@gmail.com>
2023-01-02 21:25:52 -0300

    s

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 const sections = document.querySelectorAll('section')
 const iconHome = document.querySelector('.fa-house-chimney');
 const iconUser = document.querySelector('.fa-user');
-const btnBurger = document.querySelector('.menu-burger button');
+const btnBurger = document.querySelector('.menu-burger button i');
 const list = document.querySelector('.menu-burger ul');
+const enlaces = document.querySelectorAll('nav .menu-burger ul li a');
 
 const options = {rootMargin: '-10px', threshold: "0.9"}
 
@@ -35,13 +36,26 @@ const navExpand = e =>{
 
     sections.forEach(section => section.style.display ="none")
 
-  } else{
+  }else{
     e.target.parentNode.parentNode.classList.remove('expand')
     e.target.parentNode.parentNode.classList.add('reduce')
     list.style.display = "none"
 
     sections.forEach(section => section.style.display ="flex")
-}
+  }
 }
 
+const redirect = () =>{
+   
+   btnBurger.parentNode.parentNode.classList.remove('expand')
+   btnBurger.parentNode.parentNode.classList.add('reduce')
+   list.style.display = "none"
+
+   sections.forEach(section => section.style.display ="flex")
+}
+
+
+
+
 btnBurger.addEventListener('click',navExpand)
+enlaces.forEach(a => a.addEventListener('click',redirect))
