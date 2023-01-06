9363580e29b181788de8caace5cbbe002c35fa03
Julian <julianrinaudo18@gmail.com>
2023-01-06 17:16:42 -0300

    s

--- script.js
+++ script.js
@@ -2,12 +2,22 @@
 const sections = document.querySelectorAll('section')
 const iconHome = document.querySelector('.fa-house-chimney');
 const iconUser = document.querySelector('.fa-user');
+const iconContact = document.querySelector('.fa-paper-plane');
 const iconProyect = document.querySelector('.fa-briefcase');
 const btnBurger = document.querySelector('.menu-burger button i');
 const list = document.querySelector('.menu-burger ul');
 const enlaces = document.querySelectorAll('nav .menu-burger ul li a');
 const navFIx = document.querySelector('.nav-menu');
 const menuFix = document.querySelector('.nav-fix');
+const iconMail = document.querySelector('.fa-envelope');
+const iconPhone = document.querySelector('.fa-mobile-button');
+const mail = document.querySelector('#p-mail');
+const phoneNum = document.querySelector('#p-phone');
+
+// tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 
 const options = {rootMargin: '10px', threshold: "0.9"}
 
@@ -27,6 +37,11 @@ const observer = new IntersectionObserver((entries)=> {
       }else{
         iconProyect.classList.remove('text-white');
         menuFix.style.backgroundColor = ""
+      }
+      if(e.target.id === "contact"){
+        iconContact.classList.add('text-white')
+      }else{
+        iconContact.classList.remove('text-white');
       }
     }
   })
@@ -101,8 +116,39 @@ var swiper = new Swiper(".mySwiper", {
 }
 });
 
+const changeMail = () =>{
+  if(!iconMail.classList.contains("fa-envelope-open-text")){
+    iconMail.classList.remove("fa-envelope")
+    iconMail.classList.add("fa-envelope-open-text")
+    mail.classList.remove("d-none")
+    mail.classList.add("scale-up-ver-top","p-mail")
+  }else{
+    iconMail.classList.remove("fa-envelope-open-text")
+    iconMail.classList.add("fa-envelope")
+    mail.classList.add("d-none")
+    mail.classList.remove("p-mail")
+  }
+}
+
+const changePhone = () =>{
+  if(!iconPhone.classList.contains("fa-mobile-screen-button")){
+    iconPhone.classList.remove("fa-mobile-button")
+    iconPhone.classList.add("fa-mobile-screen-button")
+    phoneNum.classList.remove("d-none")
+    phoneNum.classList.add("scale-up-ver-top","p-phone")
+  }else{
+    iconPhone.classList.remove("fa-mobile-screen-button")
+    iconPhone.classList.add("fa-mobile-button")
+    phoneNum.classList.add("d-none")
+    phoneNum.classList.remove("p-mail")
+  }
+}
+
 
 
 
 btnBurger.addEventListener('click',navExpand)
 enlaces.forEach(a => a.addEventListener('click',redirect))
+iconMail.addEventListener('click', changeMail)
+iconPhone.addEventListener('click', changePhone)
+
